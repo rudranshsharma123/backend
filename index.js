6d255a4dcede3c4c1f8f846b50d2489cde337364
Rudransh Sharma <67827010+rudranshsharma123@users.noreply.github.com>
2022-10-02 20:01:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const Stripe = require('./stripe')
+
 // const session = require('express-session')
 // var memeorystore = require('memorystore')(session)
 
@@ -20,10 +20,59 @@ const priceToProductMap = {
     regularMonthly: "price_1LoMjiFOlrjAHlmzTF4huC3K",
     regularYearly: "price_1LoMjiFOlrjAHlmzfyEifscy"
 }
+const Stripe = require("stripe")
+const stripe = Stripe("sk_test_51HufwtFOlrjAHlmz1lm6Hd3we1fj828GGSGxyuYF7t65ZoRk8FJwlwe9JR3Fe9xh8WATINP2lvfhTMyAiEgNhsVh00yLYvo96S");
+
+const addNewCustomer = async (email, desc, name) => {
+    const customer = await stripe.customers.create({
+        email: email,
+        name: name,
+        description: desc,
+    });
+    return customer;
+}
+
+const createCheckoutSession = async (customer, priceID) => {
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        customer: customer,
+        line_items: [
+            {
+                price: priceID,
+                quantity: 1,
+            },
+        ],
+        mode: 'subscription',
+        success_url: 'https://richpanel-assessment-frontend-u4x.vercel.app/success',
+        cancel_url: 'https://richpanel-assessment-frontend-u4x.vercel.app/failure',
+    });
+    return session;
+}
+
+const getCustomer = async (customerID) => {
+    const customer = await stripe.customers.retrieve(customerID);
+    return customer;
+}
+
+const cancelSubscription = async (custID) => {
+    const subscriptions = await stripe.subscriptions.list({
+        customer: custID,
+    });
+    try {
+        const subID = subscriptions["data"][0]["id"];
+        console.log(subID);
+        const session = await stripe.subscriptions.del(subID);
+        return session;
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
 
 app.post("/login", async (req, res) => {
     const { email, name, desc } = req.body;
-    const customer = await Stripe.addNewCustomer(email, desc, name);
+    const customer = await addNewCustomer(email, desc, name);
     res.send(customer);
     // res.redirect("http://localhost:3000/login");
 });
@@ -32,8 +81,8 @@ app.post("/checkout", async (req, res) => {
     const { email, name, desc, product, isMonthly } = req.body;
     const realProduct = isMonthly ? product + "Monthly" : product + "Yearly";
     const priceID = priceToProductMap[realProduct];
-    const customer = await Stripe.addNewCustomer(email, desc, name);
-    const session = await Stripe.createCheckoutSession(customer.id, priceID);
+    const customer = await addNewCustomer(email, desc, name);
+    const session = await createCheckoutSession(customer.id, priceID);
 
     res.send({ url: session.url, stripeID: customer.id, subID: session.id, sub: session });
 })
@@ -41,7 +90,7 @@ app.post("/checkout", async (req, res) => {
 app.post('/cancel', async (req, res) => {
     const { custID } = req.body;
 
-    const session = await Stripe.cancelSubscription(custID);
+    const session = await cancelSubscription(custID);
     res.send(session);
 })
 
